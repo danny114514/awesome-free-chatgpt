2c1846352af9cc17028647d95ce610ab40720b39
danny114514 <129564669+danny114514@users.noreply.github.com>
2024-08-01 10:22:41 +0800

    Update convert.py
    增加网页时识别速度。

--- convert.py
+++ convert.py
@@ -1,28 +1,44 @@
-# convert the urls in README.md to json format, and save it to urls.json, colapse the same urls, remove the last `/` in the url
-
 import json
 import re
+from concurrent.futures import ThreadPoolExecutor
 
-
-# Read the content of README.md
+# 读取README.md文件的内容
 with open("README.md", "r", encoding="utf-8") as file:
     content = file.read()
-    # Stop reading when reaching a line that contains '### 🚫 已失效'
-    content = content.split('### 🚫 已失效')[0]
-
-# Find all URLs in the content []
-urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+(?=\])', content)
-# urls = re.findall(r'(?<!~~)(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)(?!~~)', content)
-
-
-# Remove the last '/' in the URL and collapse the same URLs
+    # 当读取到包含'### 🚫 已失效'的行时停止读取
+    content = content.split('<tbody>')[1]
+    content = content.split('</tbody>')[0]
+    content = re.sub(r'\s+', ' ', content).strip()
+#print(content)
+pattern = re.compile(r'<tr.*?>(.*?)<\/tr>')
+urls = pattern.findall(content)
+print(urls[0])
+#pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+(?=\])')
+# 在内容中找到所有的URL
+#urls = pattern.findall(content)
+# 移除URL末尾的'/'并合并相同的URL
 unique_urls = []
+def det_sine(html):
+    p = re.compile(r'<td>(\d+)</td>\s*<td>\s*<a href="([^"]+)" target="_blank">[^<]+</a>\s*<br>\s*</td>\s*<td>\s*([\W\s]+)\s*</td>\s*<td>(\d{4}-\d{2}-\d{2})</td>')
+    p = p.findall(html)
+    return p
+for i in urls:
+    unique_urls.append(det_sine(i))
+    urls = []
+for i in unique_urls:
+    for h in i:
+        h=list(h)
+        h[2] = re.sub(r'\s+', '',h[2])
+        urls.append(h)
+
+print(urls)
+"""
 for url in urls:
     url = url[:-1] if url.endswith('/') else url
     if url not in unique_urls:
         unique_urls.append(url)
-
-
-# Save the URLs to urls.json
+print(unique_urls)
+"""
+# 将URL保存到urls.json文件中
 with open("urls.json", "w") as file:
-    json.dump(unique_urls, file)
+    json.dump(urls, file,indent=4)
